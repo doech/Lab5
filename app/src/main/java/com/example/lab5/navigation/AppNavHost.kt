c69cb3fb67990e8c48022d56bf08c36db26b1a46
doech <sie24405@uvg.edu.gt>
2025-09-26 09:53:08 -0600

    Estructura del laboratorio, paquetes data, ui, navegation

---
+++ app/src/main/java/com/example/lab5/navigation/AppNavHost.kt
@@ -0,0 +1,41 @@
+package com.example.lab5.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.lab5.ui.detail.PokemonDetailScreen
+import com.example.lab5.ui.detail.PokemonDetailViewModel
+import com.example.lab5.ui.list.PokemonListScreen
+import com.example.lab5.ui.list.PokemonListViewModel
+
+
+@Composable
+fun AppNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.List.route) {
+
+        composable(Routes.List.route) {
+            val vm: PokemonListViewModel = viewModel()
+            PokemonListScreen(
+                state = vm.state,
+                onItemClick = { name -> navController.navigate(Routes.Detail.create(name)) }
+            )
+        }
+
+        composable(
+            route = Routes.Detail.route,
+            arguments = listOf(navArgument("name") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val name = backStackEntry.arguments?.getString("name")!!
+            val vm: PokemonDetailViewModel = viewModel()
+            LaunchedEffect(name) { vm.load(name) }
+            PokemonDetailScreen(state = vm.state, onBack = { navController.navigateUp() })
+        }
+    }
+}
